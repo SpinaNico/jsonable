c6766e21e4006361a011ac8ae06ff891ed65d8b9
SpinaNico <spinanico93@gmail.com>
2020-01-02 17:39:12 +0100

    fix | test example #1

--- test/jmap/class_test.dart
+++ test/jmap/class_test.dart
@@ -1,16 +1,3 @@
-import "package:jsonable/jsonable.dart";
 import "../../example/lib/jmap/example_json.dart";
 
-main() {
-  var p = Person(id: 100, contact: {
-    'pc': Contact(price: 500, title: 'a01'),
-    'nb': Contact(price: 100, title: 'b01')
-  });
-  print(p.toJson());
-
-  var s =
-      """{"id":100,"contact":{"pc":{"price":500,"title":"a01"},"nb":{"price":100,"title":"b01"}}}""";
-
-  p.fromJson(s);
-  print(p.toJson());
-}
+main() {}
