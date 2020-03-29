856597e1582f820d4bdf920c12702a004a6a30f8
SpinaNico <spinanico93@gmail.com>
2020-03-29 15:55:14 +0200

    test | Jlist<Jsonble> add test

---
+++ test/jlist/jlist_jsonable_test.dart
@@ -0,0 +1,30 @@
+import 'package:jsonable/jsonable.dart';
+import 'package:test/test.dart';
+
+class PearsonWithJsonable with Jsonable {
+  JList<PearsonWithJsonable> example;
+  JNum code;
+  PearsonWithJsonable() {
+    code = jNum("code");
+    example = jList("example", builder: () => PearsonWithJsonable());
+  }
+}
+
+main() {
+  var source = """
+  {
+    "code": 0,
+    "example":[{"code": 1}, {"code": 2}]
+  }
+
+  """;
+
+  group("json recorsive test", () {
+    test("recorsive this", () {
+      var p = PearsonWithJsonable()..fromJson(source);
+      expect(p.code.getNum(), 0);
+      expect(p.example[0].code.getNum(), 1);
+      expect(p.example[1].code.getNum(), 2);
+    });
+  });
+}
